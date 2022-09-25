8c7f9ea7d7bcd2195b8900e013445b05f86f2486
Salimhan <kurull@hotmail.com>
2022-09-26 02:09:06 +0300

    puppeteer added

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
-import axios from 'axios'
+import puppeteer from 'puppeteer';
 
 (async () => {
-    console.log('Hello world Test')
-})()
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.binance.com/en/trade/BUSD_TRY?theme=dark&type=spot');
+
+  const delta = await page.evaluate(() => {
+    return document.querySelectorAll("#__APP > div.css-4pd5zw > div > div.css-1cpzhey > div > div > div.left > div > div.nowPrice > div.subPrice")[0].textContent
+  });
+
+  console.dir(delta)
+  await browser.close();
+})();
+
+
 
