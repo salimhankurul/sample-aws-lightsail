52e0f3d58a84c12440a5950560a43adc2a172521
Salimhan <kurull@hotmail.com>
2022-09-26 02:48:23 +0300

    turned into server

--- index.ts
+++ index.ts
@@ -1,28 +1,26 @@
 // run puppeteer on VC2 https://github.com/alixaxel/chrome-aws-lambda/issues/164#issuecomment-754621407
-
 import puppeteer from 'puppeteer';
+//@ts-ignore
 import express from 'express';
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox'] });
-  const page = await browser.newPage();
-  await page.goto('https://www.binance.com/en/trade/BUSD_TRY?theme=dark&type=spot');
+const app = express()
+
+app.get('/getData', async (req: any, res: any) => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox'],
+  })
+  const page = await browser.newPage()
+  await page.goto('https://www.binance.com/en/trade/BUSD_TRY?theme=dark&type=spot')
 
   const delta = await page.evaluate(() => {
-    return document.querySelectorAll("#__APP > div.css-4pd5zw > div > div.css-1cpzhey > div > div > div.left > div > div.nowPrice > div.subPrice")[0].textContent
-  });
+    return document.querySelectorAll('#__APP > div.css-4pd5zw > div > div.css-1cpzhey > div > div > div.left > div > div.nowPrice > div.subPrice')[0].textContent
+  })
 
   console.dir(delta)
-  await browser.close();
-})();
+  await browser.close()
 
-
-const app = express();
-
-app.get('/', (req: any, res: any) => {
-    res.send('This is a test web page!');
+  res.send(delta)
 })
 
 app.listen(3000, () => {
